d6172b91546462b31ec45fd1e0b11d22c6b87c28
Herve Tchepannou <herve.tchepannou@wutsi.com>
2022-11-09 16:59:41 -0500

    update platform version

--- src/main/kotlin/com/wutsi/checkout/access/endpoint/UpdateOrderBalanceController.kt
+++ src/main/kotlin/com/wutsi/checkout/access/endpoint/UpdateOrderBalanceController.kt
@@ -1,9 +1,10 @@
 package com.wutsi.checkout.access.endpoint
 
-import com.wutsi.checkout.access.delegate.UpdateOrderBalanceDelegate
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RestController
+import com.wutsi.checkout.access.`delegate`.UpdateOrderBalanceDelegate
+import org.springframework.web.bind.`annotation`.PathVariable
+import org.springframework.web.bind.`annotation`.PostMapping
+import org.springframework.web.bind.`annotation`.RestController
+import kotlin.String
 
 @RestController
 public class UpdateOrderBalanceController(
